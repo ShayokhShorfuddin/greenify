8c1e09901d3b3572f849cdce8a93b2ff468261db
Shayokh <shayokhshuvro14@gmail.com>
2025-10-07 16:52:10 +0600

    add more FAQ

--- app/_components/FAQ.tsx
+++ app/_components/FAQ.tsx
@@ -3,17 +3,31 @@ import Image from "next/image";
 import undraw_tree from "@/public/svgs/undraw_tree.svg";
 import AnimatedAccordionItem from "./AnimatedAccordionItem";
 
-// TODO: Add more FAQs
 const information = [
+  {
+    header: "Is this service free to use?",
+    content:
+      "Yes, our service is completely free to use. We believe in making web sustainability accessible to everyone. However, we would really appreciate contributions to improve the platform and keep it available ",
+  },
+  {
+    header: "How accurate are the generated results?",
+    content:
+      "We strive for high accuracy, but results may vary based on multiple factors, such as internet connectivity, host server configurations, availability of our third-party services, etc. Consider auditing sites every now and then to ensure everything is in order.",
+  },
+  {
+    header: "Are the generated results stored or shared?",
+    content:
+      "We only cache successful audit's results in our backend for 6 hours in order to prevent API abuse and server exhaustion. If you would like to save an audit result temporarily, we offer you an option to save upto 5 results in your browser. Except for that, we do not store or share any of the generated results.",
+  },
   {
     header: "There are existing solutions out there. Why another one?",
     content:
       "We are aware of existing solutions and, in fact, we use some of them ourselves. However, none offer a feature-rich, open-source, complete solution. Think of it as scattered pieces of a puzzle that we are determined to put together.",
   },
   {
-    header: "How accurate are the generated results?",
+    header: "Can I audit password-protected or localhost sites?",
     content:
-      "We strive for high accuracy, but results may vary based on multiple factors, such as internet connectivity, host server configurations, availability of our third-party services, etc. Consider auditing sites every now and then to ensure everything is in order.",
+      "Currently, we only support publicly accessible websites. For security and technical reasons, auditing password-protected or localhost sites is not available at this time.",
   },
 ];
 
@@ -44,7 +58,7 @@ export default function FAQ() {
         <Image
           src={undraw_tree}
           alt="Tree Illustration"
-          className="hidden xs:block absolute right-8 xs:top-0 sm:top-1 size-8"
+          className="hidden xs:block absolute right-9 xs:top-0 sm:top-1 size-8"
         />
       </div>
     </section>
